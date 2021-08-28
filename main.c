7604fa47c55d10ac7b251f05c340e14d896a34da
Juan Pablo Noreña Mejia <juan.norena@upb.edu.co>
2021-08-28 15:59:00 -0500

    puntos uno y dos

--- main.c
+++ main.c
@@ -4,5 +4,26 @@
 
 int main(int argc, char *argv[]){
 
+    FILE *fin = fopen("./fin","r");
+    if(fin == NULL){
+        perror("Error al abrir archivo de entrada: \n");
+        return (EXIT_FAILURE);
+    }
+
+    FILE *fout = fopen("./fout","a+");
+    if(fout == NULL){
+        perror("Error al abrir archivo de salida: \n");
+        return (EXIT_FAILURE);
+    }
+
+    for (int i = 0; i < 6; i++)
+    {
+        if (i == 1) feature1(fin, fout);
+        if (i == 2) feature2(fin, fout);
+    }
+
+    fclose(fin);
+    fclose(fout);
+    
     return EXIT_SUCCESS;
 }
--- student.c
+++ student.c
@@ -1,3 +1,33 @@
 #include <stdlib.h>
 #include "student.h"
 
+void feature1(FILE *fin, FILE *fout) {
+    
+    char buffer[64];
+    char *status = NULL;
+
+    status = fgets(buffer, sizeof(buffer), fin);
+    if (status != NULL) {
+        fprintf(fout, "%s", buffer);
+    }
+    printf("\n");
+
+    fflush(fin);
+}
+
+void feature2(FILE *fin, FILE *fout) {
+    
+    char buffer[70];
+    char *status = NULL;
+
+    status = fgets(buffer, sizeof(buffer), fin);
+    if (status != NULL) {
+        for (int i = sizeof(buffer) - 2; i >= 0; i--) {
+            fprintf(fout, "%c", buffer[i]);
+        }
+    }
+    
+    printf("\n");
+
+    fflush(fin);
+}
